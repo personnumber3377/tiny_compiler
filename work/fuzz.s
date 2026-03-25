827ff38e924001220eaf2821149c78d06e3887b0
personnumber3377 <sontapaa.jokulainen@gmail.com>
2026-03-25 13:06:18 +0200

    Blah

--- work/fuzz.s
+++ work/fuzz.s
@@ -1,30 +1,18 @@
-mov $0, 7
-mov $1, 6
-mov $2, 7
-mov $3, 10
+mov $0, 0
+mov $1, 5
+mov $2, 2
+mov $3, 1
 mov $4, 5
-mov $5, 0
-mov $6, 2
-mov $7, 4
-cmp $5, $3
-bge >ifend0
-add $1, $4, 5
-@ifend0:
-mov $4, 20
-mov $2, 0
-@whilestart1:
-cmp $2, 2
-bge >whileend2
-mov $7, 18
-add $2, $2, 1
-jmp >whilestart1
-@whileend2:
-mov $2, 0
-@whilestart3:
-cmp $2, 4
-bge >whileend4
-add $7, $6, 0
-add $2, $2, 1
-jmp >whilestart3
-@whileend4:
+mov $5, 3
+mov $6, 7
+mov $7, 8
+cmp $3, $1
+bge >ifend19
+add $7, $4, 1
+@ifend19:
+mov $7, 13
+cmp $5, $5
+bge >ifend20
+add $2, $7, 2
+@ifend20:
 hlt
